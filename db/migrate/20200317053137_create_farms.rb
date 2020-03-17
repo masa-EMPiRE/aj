e901efd2750a75210c49cf3d71f51d9b647d0bca
谷崎公紀 <marutto8@gmail.com>
2020-03-17 16:07:10 +0900

    first commit

---
+++ db/migrate/20200317053137_create_farms.rb
@@ -0,0 +1,12 @@
+class CreateFarms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :farms do |t|
+      t.string :name
+      t.text :description
+      t.string :email
+      t.text :image_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+Farm.create(
+  name: "小谷農場",
+  description: "がんばってます！！",
+  email: "odod.gmail.com",
+  image_url: "https://smartagri-jp.com/images/upload/2018/07/025bcd40b47058e238ffc68a1d436256.jpg"
+)
+
+Farm.create(
+  name: "NomuraFarm",
+  description: "イギリスっぽくしたい",
+  email: "nomu@gmail.com",
+  image_url: "https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/75066653_2661183763975825_9138477571548119040_n.jpg?_nc_cat=100&_nc_sid=85a577&_nc_ohc=f_WRwaKNH4cAX9RPkak&_nc_ht=scontent-nrt1-1.xx&oh=d9608e04ee60f8d127c5fbc85edc10b3&oe=5E9433CE"
+)
+
+Farm.create(
+  name: "中村浩の畑",
+  description: "おれやで",
+  email: "hirobomb@gmail.com",
+  image_url: "https://pbs.twimg.com/profile_images/1047023952013475841/FR6qEJkH_400x400.jpg"
+)
